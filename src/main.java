79e1f88c74a0c549aafd39cfeb7665f774c6c0eb
AleksandrStupnikov <a-a-stupnikov@yandex-team.ru>
2024-07-01 10:40:38 +0300

    hw2.1

---
+++ src/main.java
@@ -0,0 +1,10 @@
+public class Task1 {
+    public static void main(String[] args) {
+
+           int TicketPrice = 74_569;
+           int Mile = 20;
+           int BonusMile = TicketPrice / Mile;
+        System.out.println ("У Вас "+ BonusMile  +" бонусных милей." );
+
+    }
+}
